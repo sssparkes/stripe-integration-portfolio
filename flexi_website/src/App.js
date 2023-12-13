a7c988bbc4dc5dbfeffe180ee18b63e1905ee239
Spencer Parkes <sssparkes@gmail.com>
2023-12-13 01:10:10 -0800

    stripe now working

--- flexi_website/src/App.js
+++ flexi_website/src/App.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import './css/App.css';
 import Home from './component/Home';
 import About from './component/About';
 import SignUp from './component/SignUp';
+
+// Replace 'your-publishable-key' with your actual Stripe publishable key
+const stripePromise = loadStripe('pk_test_51NwshIGPvFriD0ZoWyBOcjgQ8evCfJ8hJ9hs0lLP8QfPb1ldAXeYB0O16PPpHO5vXsnx8veyfsB2EVCGMCPrSLZr00am8tEQab');
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-        
           <h1>Your Company Name</h1>
           <nav>
             <Link className="App-link" to="/">Home</Link>
             <Link className="App-link" to="/about">About</Link>
             <Link className="App-link" to="/signup">Sign Up</Link>
-         
           </nav>
         </header>
         <div className="Content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/signup" element={<SignUp />} />
-            {/* Add more routes for other pages */}
-          </Routes>
+          {/* Wrap your Routes with Elements provider */}
+          <Elements stripe={stripePromise}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/signup" element={<SignUp />} />
+              {/* Add more routes for other pages */}
+            </Routes>
+          </Elements>
         </div>
       </div>
     </Router>
--- flexi_website/src/component/SignUp.js
+++ flexi_website/src/component/SignUp.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Import axios for making HTTP requests
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import axios from 'axios';
 
 const SignUp = () => {
   const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
 
   const handlePayment = async () => {
     try {
       setLoading(true);
 
-      // Make a request to your backend to create a payment intent
-      const response = await axios.post('http://localhost:5000/create-payment-intent');
-      const clientSecret = response.data.clientSecret;
-
-      // Use Stripe.js to handle the payment
-      const stripe = window.Stripe('pk_test_51NwshIGPvFriD0ZoWyBOcjgQ8evCfJ8hJ9hs0lLP8QfPb1ldAXeYB0O16PPpHO5vXsnx8veyfsB2EVCGMCPrSLZr00am8tEQab'); // Replace 'your-publishable-key' with your actual Stripe publishable key
-      const { error } = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-          card: stripe.elements.getElement('card'), // Replace with your actual card element reference
-        },
+      // Create a PaymentMethod object using card element
+      const cardElement = elements.getElement(CardElement);
+      const { paymentMethod, error } = await stripe.createPaymentMethod({
+        type: 'card',
+        card: cardElement,
       });
 
       if (error) {
         console.error(error);
         alert('Payment failed. Please try again.');
       } else {
-        alert('Payment successful!');
+        // Make a request to your backend to create a payment intent
+        const response = await axios.post('http://localhost:5000/create-payment-intent', {
+          payment_method: paymentMethod.id,
+        });
+
+        const clientSecret = response.data.clientSecret;
+
+        // Use Stripe.js to handle the payment
+        const confirmPayment = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: paymentMethod.id,
+        });
+
+        if (confirmPayment.error) {
+          console.error(confirmPayment.error);
+          alert('Payment failed. Please try again.');
+        } else {
+          alert('Payment successful!');
+        }
       }
     } catch (err) {
       console.error(err);
@@ -37,6 +52,9 @@ const SignUp = () => {
   return (
     <div>
       <h2>Pre-Purchase the Beta Version</h2>
+
+      {/* Add the CardElement for collecting card details */}
+      <CardElement />
 
       {/* Add your button with an onClick event to trigger the payment */}
       <button onClick={handlePayment} disabled={loading}>
